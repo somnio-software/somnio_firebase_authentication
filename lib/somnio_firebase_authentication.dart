1fd12451322b54f043cba8bac56577480a80bcc7
Federico Perez <fperez@somniosoftware.com>
2021-06-22 11:19:33 -0300

    feat/Added parts and imports

--- lib/somnio_firebase_authentication.dart
+++ lib/somnio_firebase_authentication.dart
@@ -1,7 +1,13 @@
 library somnio_firebase_authentication;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'src/sign_in_services/apple/apple_sign_in_service.dart';
+import 'src/sign_in_services/facebook/facebook_sign_in_service.dart';
+import 'src/sign_in_services/google/googe_sign_in_service.dart';
+import 'src/sign_in_services/sign_in_service.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+part './src/auth_service.dart';
+part './src/firebase_auth_service.dart';
