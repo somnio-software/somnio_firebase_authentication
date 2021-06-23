376548aef9b3dbb40fbb7c7c90c6185c6e8c72ae
Federico Perez <fperez@somniosoftware.com>
2021-06-23 10:07:07 -0300

    feat/ new Export

--- lib/somnio_firebase_authentication.dart
+++ lib/somnio_firebase_authentication.dart
@@ -10,5 +10,8 @@ import 'src/sign_in_services/sign_in_service.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+export 'package:somnio_firebase_authentication/somnio_firebase_authentication.dart'
+    show AuthService, FirebaseAuthService;
+
 part 'src/auth_service.dart';
 part 'src/firebase_auth_service.dart';
