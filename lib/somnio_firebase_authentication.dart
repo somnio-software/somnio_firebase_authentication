15404ce1ec6e7570f536d4ba51d481ae30c6a7cd
Federico Perez <fperez@somniosoftware.com>
2021-06-23 11:19:18 -0300

    test

--- lib/somnio_firebase_authentication.dart
+++ lib/somnio_firebase_authentication.dart
@@ -1,16 +1,16 @@
 library somnio_firebase_authentication;
 
-import 'dart:async';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'dart:async';
+// import 'package:firebase_auth/firebase_auth.dart';
 
-import 'src/sign_in_services/apple/apple_sign_in_service.dart';
-import 'src/sign_in_services/facebook/facebook_sign_in_service.dart';
-import 'src/sign_in_services/google/google_sign_in_service.dart';
-import 'src/sign_in_services/sign_in_service.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+// import 'src/sign_in_services/apple/apple_sign_in_service.dart';
+// import 'src/sign_in_services/facebook/facebook_sign_in_service.dart';
+// import 'src/sign_in_services/google/google_sign_in_service.dart';
+// import 'src/sign_in_services/sign_in_service.dart';
+// import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
 
 export 'src/sign_in_services/google/google_sign_in_service.dart';
 
-part 'src/auth_service.dart';
-part 'src/firebase_auth_service.dart';
+// part 'src/auth_service.dart';
+// part 'src/firebase_auth_service.dart';
--- lib/src/sign_in_services/google/google_sign_in_service.dart
+++ lib/src/sign_in_services/google/google_sign_in_service.dart
@@ -1,3 +1,5 @@
+library google_sign_in_service;
+
 import '../sign_in_service.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart' as Auth;
